e4006ea8277ac05138b03addce08d8fa8f3c4d6f
aidanaden <aidanaden@hotmail.com>
2022-06-27 00:37:14 +0800

    chore: rebuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,3 @@
-// import type { Component } from 'solid-js';
-
-// const App: Component = () => {
-//   return (
-//     <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-//   );
-// };
-
-// export default App;
-
 import {
   Component,
   createEffect,
